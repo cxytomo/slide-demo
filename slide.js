3eb57f993cf0a10e9547119190b6224acb45ac91
tomo <cxytomo@gmail.com>
2012-12-19 12:39:32 +0800

    final version
    slide smoothly

--- slide.js
+++ slide.js
@@ -2,23 +2,36 @@
 1.实现无移出效果时的切换（2个小时，太无能了）
 2.实现移出效果的切换
 */
-var interv;
+var interv
+	, flag = 1;
 function circle () {
 	interv = setInterval(function(){
-	var $active = $('.carousel').find('.item.active')
-		, children = $active.parent().children()
-		, activePos = children.index($active)
+	var $active
+		, children
+		, activePos
 		, $next;
+		if(flag === 1) {
+			$active = $('.carousel').find('.item.initial');
+		}
+		else {
+			$active =  $('.carousel').find('.item.active');
+		}
+		children = $active.parent().children();
+		activePos = children.index($active);
 		if(activePos < (children.length - 1) && activePos > -1) {
 			$next = children[activePos + 1];
 		}
 		else if(activePos === children.length - 1) {
 			$next = children[0];
 		}
-		$active.fadeTo('slow', 0.01, function(){
+		$active.fadeTo('slow', 0, function(){
+			flag = 0;
+			$active[0].style.display = 'none';
+			$active[0].style.opacity = 1;
+			$active.removeClass('initial');
 			$active.removeClass('active');
 			$next.className = 'item active';
-			$next.style.opacity = 1;
+			$('.carousel').find('.item.active').fadeIn('slow');
 		});		
 	}, 5000);
 }
@@ -37,11 +50,15 @@ var $active = $('.carousel').find('.item.active')
 		else if(activePos === children.length - 1) {
 			$next = children[0];
 		}
-		$active.fadeTo('slow', 0.01, function(){
+		$active.fadeTo('slow', 0, function(){
+			flag = 0;
+			$active[0].style.display = 'none';
+			$active[0].style.opacity = 1;
+			$active.removeClass('initial');
 			$active.removeClass('active');
 			$next.className = 'item active';
-			$next.style.opacity = 1;
-		});
+			$('.carousel').find('.item.active').fadeIn('slow');
+		});	
 	}
 	if(direction === 'prev') {
 		if(activePos > 0 && activePos <= (children.length - 1)){
@@ -50,14 +67,17 @@ var $active = $('.carousel').find('.item.active')
 		else if(activePos === 0) {
 			$prev = children[children.length - 1];
 		}
-		$active.fadeTo('slow', 0.01, function(){
+		$active.fadeTo('slow', 0, function(){
+			flag = 0;
+			$active[0].style.display = 'none';
+			$active[0].style.opacity = 1;
+			$active.removeClass('initial');
 			$active.removeClass('active');
 			$prev.className = 'item active';
-			$prev.style.opacity = 1;
+			$('.carousel').find('.item.active').fadeIn('slow');
 		});
 	}	
-	return this;
-	}
+}
 
 $('.carousel').mouseover(function () {
 	clearInterval(interv);
