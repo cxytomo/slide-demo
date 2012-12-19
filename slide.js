900f95d75ab47b57373348f5ba1fe93d9176a5de
tomo <cxytomo@gmail.com>
2012-12-19 13:26:45 +0800

    typeset
    typeset

--- slide.js
+++ slide.js
@@ -3,45 +3,45 @@
 2.实现移出效果的切换
 */
 var interv
-	, flag = 1;
+, flag = 1;
 function circle () {
 	interv = setInterval(function(){
-	var $active
+		var $active
 		, children
 		, activePos
 		, $next;
-	if(flag === 1) {
-		$active = $('.carousel').find('.item.initial');
-	}
-	else {
-		$active =  $('.carousel').find('.item.active');
-	}
-	children = $active.parent().children();
-	activePos = children.index($active);
-	if(activePos < (children.length - 1) && activePos > -1) {
-		$next = children[activePos + 1];
-	}
-	else if(activePos === children.length - 1) {
-		$next = children[0];
-	}
-	$active.fadeTo('slow', 0, function(){
-		flag = 0;
-		$active[0].style.display = 'none';
-		$active[0].style.opacity = 1;
-		$active.removeClass('initial');
-		$active.removeClass('active');
-		$next.className = 'item active';
-		$('.carousel').find('.item.active').fadeIn('slow');
-	});		
+		if(flag === 1) {
+			$active = $('.carousel').find('.item.initial');
+		}
+		else {
+			$active =  $('.carousel').find('.item.active');
+		}
+		children = $active.parent().children();
+		activePos = children.index($active);
+		if(activePos < (children.length - 1) && activePos > -1) {
+			$next = children[activePos + 1];
+		}
+		else if(activePos === children.length - 1) {
+			$next = children[0];
+		}
+		$active.fadeTo('slow', 0, function(){
+			flag = 0;
+			$active[0].style.display = 'none';
+			$active[0].style.opacity = 1;
+			$active.removeClass('initial');
+			$active.removeClass('active');
+			$next.className = 'item active';
+			$('.carousel').find('.item.active').fadeIn('slow');
+		});		
 	}, 5000);
 }
 
 function slide (direction) {	
 	var $active = $('.carousel').find('.item.active')
-		, children = $active.parent().children()
-		, activePos = children.index($active)
-		, $next
-		, $prev;
+	, children = $active.parent().children()
+	, activePos = children.index($active)
+	, $next
+	, $prev;
 	direction = direction || 'next';
 	if(direction === 'next') {
 		if(activePos < (children.length - 1) && activePos > -1) {
@@ -82,13 +82,16 @@ function slide (direction) {
 $('.carousel').mouseover(function () {
 	clearInterval(interv);
 });
+
 $('.carousel').mouseleave(function () {
 	circle();
 });
+
 $('.carousel-control.left').click(function(e){
 	slide('prev');
 	e.preventDefault();
 });
+
 $('.carousel-control.right').click(function(e){
 	slide('next');
 	e.preventDefault();
